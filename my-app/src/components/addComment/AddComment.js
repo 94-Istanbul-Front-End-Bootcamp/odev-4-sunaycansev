59800bcb219f2a3af51342f009f46dfe67548876
sunaycansev <fsunaycansev@gmail.com>
2021-07-09 15:00:39 +0300

    add assignment-4

---
+++ my-app/src/components/addComment/AddComment.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactStars from "react-rating-stars-component";
+
+export default function AddComment(props) {
+  console.log(props.value);
+
+  return (
+    <div className="comment-form">
+      <h3>Yorum Ekle</h3>
+      <textarea
+        placeholder="Yorumunuz.."
+        rows="5"
+        cols="28"
+        value={props.value}
+        onChange={props.onInputChange}
+      />
+      <br />
+      <ReactStars
+        count={5}
+        onChange={props.ratingChanged}
+        value={props.vote}
+        size={30}
+        activeCoor="#fd700"
+      />
+    </div>
+  );
+}
